854e14005ca2cf56624369e252303be4f435e501
MD NASIR UDDIN <45711989+Leanneee@users.noreply.github.com>
2020-08-15 02:01:46 +0600

    Add files via upload

---
+++ javascripts/application-1591944720.js
@@ -0,0 +1,11 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+
+// $(".auto_search_complete").autocomplete({
+//     source: "autocomplete.js",
+//     minLength: 3
+// });
+
+//= require bootstrap
+//= require bootstrap-sprockets
